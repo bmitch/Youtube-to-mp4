830a2f2e6ce296efc44891dface4a244a4cc858a
Bill <wkmitch@gmail.com>
2013-11-23 14:06:36 -0800

    first commit

---
+++ top5.py
@@ -0,0 +1,20 @@
+import json
+import os
+
+
+
+
+json_data = open("top5.json").read()
+data = json.loads(json_data)
+data = data["feed"]
+data = data['data']
+
+print data[1]['link']
+
+
+for (i, item) in enumerate(data):
+	if 'link' in data[i]:
+		print " " + str(i) + " " + data[i]['link']
+		print str(i) + "/" + str(len(data))
+		os.system("youtube-dl " + data[i]['link'])
+		os.system("sudo mv *.mp4 /mnt/videos/youtubemp4s")
